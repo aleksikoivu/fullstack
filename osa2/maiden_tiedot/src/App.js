660f78d6263cf1938c5258ccf03cacc1f2e75781
Aleksi <aleksikoivu2@kamk.fi>
2020-07-01 16:21:52 +0300

    osa 2 maat done

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,22 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 
-// api key 9696232abba9dba5ee3666e10dd2e768
-// api endpoint http://api.weatherstack.com/
+const weatherApiKey = process.env.REACT_APP_API_KEY
 
-// Current Weather API Endpoint
+const weatherQuery = async (city) => {
+  //console.log('weather queryssä')
+  //console.log(city)
+  const params = {
+    access_key: weatherApiKey,
+    query: city
+  }
 
-//http://api.weatherstack.com/current
-    // ? access_key = YOUR_ACCESS_KEY //tähän siis api key
-    // & query = New York
-    
-// var YOUR_ACCESS_KEY = "9696232abba9dba5ee3666e10dd2e768";
-// optional parameters: 
+  const response = await Axios.get('http://api.weatherstack.com/current',{params})
+  // const response = await Axios.get('http://api.weatherstack.com/current?access_key='+params.access_key+'&query='+params.city)
+  console.log(response.data)
 
-    // & units = m
-    // & language = en
-    // & callback = MY_CALLBACK
+  const temperature = response.data.current.temperature
+  return temperature
+}
 
+const Weather = (props) => {
+  const [weather, setWeather] = useState([])
+
+  var capital = props.capital
+  weatherQuery(capital).then(result => setWeather(result))
+  
+
+  if(weather === undefined){
+    return( 
+      <div>Loading</div>
+    )
+  }else{
+    const temperature = weather
+    return(
+      <div>Temperature: {temperature}</div>
+
+    )
+  }
+  
+}
 
 const FilterCountries = (props) => {
   return(
@@ -28,6 +50,7 @@ const FilterCountries = (props) => {
 
 const PrintCountries = (props) => {
   var matches = props.data.length
+  var content = null
   //console.log(props.data.length)
   //console.log(props.data[0])
 
@@ -38,20 +61,19 @@ const PrintCountries = (props) => {
   }
 
   else if(matches < 10 && matches > 1){ //jos alle 10 mutta yli 1 (tiedot tänne)
+    content = props.data.map(country => 
+      <ul key={country.id}>
+        {country.name} <button value={country.name} onClick={props.showMoreHandle} >Show more</button>
+      </ul>)
+    
     return(
-      <div>
-        {props.data.map(country=> 
-        <ul key={country.id}>
-          {country.name} <button value={country.name} onClick={props.showMoreHandle} >Show more</button>
-        </ul>)}
-      </div>
+      <div>{content}</div>
     )
   }
 
-
   // maa h1, pk & population, kielet & lippu
   else if(matches === 1){ // jos 1 vastaavuus (maa yksityiskohtaisesti tänne)
-    const content = props.data.map(country =>
+    content = props.data.map(country =>
       <div key={country.id}>
         <h1>{country.name}</h1>
         <ul>Capital - {country.capital}</ul>
@@ -66,15 +88,9 @@ const PrintCountries = (props) => {
         <picture>
           <img src={country.flag} widht="350" height="250" alt="flag"></img>
         </picture>
+
+        <Weather capital={country.capital}/>
       </div>)
-
-    // console.log(props.data)
-    // console.log(props.data[0].capital) //tää on se kutsu jolla saa pk ulos
-      
-    // ajetaan semi epäortodoksista(?) reittiä sääinfot :D
-
-    props.weather(props.data[0].capital) 
-
 
     return(
       <div>
@@ -94,7 +110,6 @@ const PrintCountries = (props) => {
 const App = () => {
   const [countries, setCountries] = useState([])
   const [countriesToShow, setCountriesToShow] = useState([])
-  const [weatherData, setWeatherData] = useState([])
 
   useEffect(() => {
     console.log("effect")
@@ -127,52 +142,34 @@ const App = () => {
     }
   }
 
-  const weatherQuery = (city) => {
-    const params = {
-      access_key: '9696232abba9dba5ee3666e10dd2e768',
-      query: city
-    }
-    var access_key = "9696232abba9dba5ee3666e10dd2e768"
-    // kaikista parastahan tässä on ehkä se että kumpikaan noista axios geteistä ei toimi yksin, mutta yhdessä kyllä
-    Axios.get('http://api.weatherstack.com/current?access_key='+access_key+"&query="+city)
-    Axios.get('http://api.weatherstack.com/current', {params})
-    .then(response=>{
-      console.log(response.data)
-      const w = response.data
-      setWeatherData(w)
-    })
-    
-    // //api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
-    // const params = {
-    //     appid: '1c7640f8d629370f45fda3b23a7b8844',
-    //     q: city
-    //   }
-    // var id = '1c7640f8d629370f45fda3b23a7b8844'
-    // var query = 'London'
-    // Axios.get('api.openweathermap.org/data/2.5/weather?q=Helsinki&appid=1c7640f8d629370f45fda3b23a7b8844')
-    // .then(response=>{
-    //   console.log(response.data)
-    // }) 
-  }
 
-  // const testi = (event) => {
-  //   console.log("triggered")
-  //   // Axios.get('http://api.weatherstack.com/current?access_key=9696232abba9dba5ee3666e10dd2e768&query=New%20York'
-  //   // )
-  //   // .then(response => {
-  //   //   console.log("Promise fulfilled")
-  //   //   console.log(response.data)
-  //   // })
-  //   weatherQuery("Helsinki")
+
+  //   var access_key = weatherApiKey
+  //   // kaikista parastahan tässä on ehkä se että kumpikaan noista axios geteistä ei toimi yksin, mutta yhdessä kyllä
+  //   Axios.get('http://api.weatherstack.com/current?access_key='+access_key+"&query="+city)
+  //   Axios.get('http://api.weatherstack.com/current', {params})
+  //   .then(response=>{
+  //     console.log(response.data)
+  //     const w = response.data
+  //     setWeatherData(w)
+  //   })
   // }
-
+    // Current Weather API Endpoint
+//http://api.weatherstack.com/current
+    // ? access_key = YOUR_ACCESS_KEY //tähän siis api key
+    // & query = New York
+// optional parameters: 
+    // & units = m
+    // & language = en
+    // & callback = MY_CALLBACK
+  
   return (
     <div>
       <h2>Countries</h2>
 
       <FilterCountries Filter={Filter}/>
 
-      <PrintCountries weatherData={weatherData} data={countriesToShow} showMoreHandle={Filter} weather={weatherQuery} />
+      <PrintCountries data={countriesToShow} showMoreHandle={Filter} />
 
       {/* <button onClick={testi}>klikki</button> */}
     
